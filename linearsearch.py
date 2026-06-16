5f2863068ac0e5946865d02335f4adcd8a2cc55b
Chewww <chupannuchal@gmail.com>
2026-06-16 20:52:46 +0630

    second commit

--- linearsearch.py
+++ linearsearch.py
@@ -5,5 +5,9 @@ def linearSearch(array,search_item):
     return None
 number_array= [1,4,45,90,36,21,43,88,17,16,5]
 result=linearSearch(number_array,16)
-print("The search item found in index number: ",result)
+
+if result==None:
+    print("Element is not found")
+else:
+    print("The search item found in index number: ",result)
             
